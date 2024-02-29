6bdb4f0fc6b825c39babcf269cb2baee3b9c886d
ByteBlink <jaakupienes@gmail.com>
2024-02-29 01:05:05 +0100

    feat: Dashboard updated.

--- ontrial/client/src/App.jsx
+++ ontrial/client/src/App.jsx
@@ -1,7 +1,7 @@
 import { ChakraProvider, Container } from '@chakra-ui/react';
 import Dashboard from './components/Dashboard';
 import Navbar from './components/Navbar';
-import SubscriptionList from './components/SubscriptionList';
+
 
 function App() {
   return (
@@ -9,7 +9,6 @@ function App() {
       <Container maxW="container.xl" px={20}>
         <Navbar/>
         <Dashboard />
-        <SubscriptionList/>
       </Container>
     </ChakraProvider>
   );
--- ontrial/client/src/components/Dashboard.jsx
+++ ontrial/client/src/components/Dashboard.jsx
@@ -1,12 +1,29 @@
-
+import {Box, Flex, Button, Text} from '@chakra-ui/react'
+import SubscriptionList from './SubscriptionList';
+import mockSubscriptions from '../mockSubscriptions';
 
 const Dashboard = () => {
+  // Calculate average costs per month 
+  const activeSubscriptions = mockSubscriptions.filter(sub => sub.status === 'Active');
+  const totalCost = activeSubscriptions.reduce((acc, curr) => acc + curr.cost, 0);
+  const averageExpenses = activeSubscriptions.length > 0 ? (totalCost / activeSubscriptions.length).toFixed(2) : '0.00';
 
 
   return (
-    <>
-    Dashboard Component!
-    </>
+    <Box>
+      <Flex justifyContent="space-between" alignItems="center" p={4}>
+        <Text fontSize="2xl" fontWeight="bold">Subscriptions</Text>
+        <Button colorScheme="teal" onClick={() => console.log('Add Subscription')}>Add Subscription</Button>
+      </Flex>
+      <SubscriptionList/>
+      <Flex justifyContent="space-between" alignItems="center" p={4} bg="gray.200" bgOpacity="0.8" borderRadius="md" mt={7} ml={2} mr={2}>
+        <Box>
+          <Text fontSize="lg">Average Expenses</Text>
+          <Text fontSize="sm" as="i" fontStyle="italic">per month</Text>
+        </Box>
+        <Text fontSize="lg">${averageExpenses}</Text>
+      </Flex>
+    </Box>
   );
 };
 
